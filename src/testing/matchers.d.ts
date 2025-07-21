43b97ecb88a9d21d8547a56a63879ebb125932aa
Roy Portas <royportas@gmail.com>
2025-07-21 14:26:32 +1000

    Fix build

--- src/testing/matchers.d.ts
+++ src/testing/matchers.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 import { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
 import { Matchers, AsymmetricMatchers } from "bun:test";
 
